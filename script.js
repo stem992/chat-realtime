fcf9b57f846b282b811e37141255ff88453e84d0
Stefano <stemeli92@gmail.com>
2023-10-08 15:26:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const chatMessages = document.getElementById('chat-messages'); 
+    const messageInput = document.getElementById('message-input'); 
+    const sendButton = document.getElementById('send-button');
+
+    sendButton.addEventListener('click', () => {
+        const messageText = messageInput.value.trim(); // rimozione spazi
+
+        if (messageText !== '') {
+            const messageElement = document.createElement('div');
+            messageElement.classList.add('message', 'sent'); 
+            messageElement.textContent = messageText; 
+            chatMessages.appendChild(messageElement); 
+
+            setTimeout(() => {
+                const responseElement = document.createElement('div');
+                responseElement.classList.add('message', 'received'); 
+                if(messageText == 'ciao'){
+                    responseElement.textContent = 'ciao a te!';
+
+                } else if(messageText == 'come ti chiami?'){
+                    responseElement.textContent = 'stefano';
+                    
+                }  else {
+                    responseElement.textContent = 'ricevuto!';
+                }
+               
+                chatMessages.appendChild(responseElement); 
+                chatMessages.scrollTop = chatMessages.scrollHeight; 
+            }, 1000); 
+
+
+            messageInput.value = '';
+
+            chatMessages.scrollTop = chatMessages.scrollHeight;
+        }
+    });
+
+    messageInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault(); 
+            sendButton.click(); 
+        }
+    });
+});
